2e55f34b1676f2cfdf1556b8df5d4a4965265236
Yash Jain <yash.jain6350@gmail.com>
2019-05-09 00:43:37 +0530

    basics of vectors

---
+++ basic_vectors_functionality.R
@@ -0,0 +1,28 @@
+# adding vectors
+a <- 1:5
+b <- 1:3
+a+b
+
+
+a <- 1:10
+
+# return true or false or every number according to condition
+a < 5
+
+# any() method it test all the numbers form 1 to 10 if any-one of the number is less than 5,  it return true
+any(a < 5)
+
+# all() method return true if all of the number are less than 5
+all(a < 5)
+
+
+# getting num form specific position in the vector
+vector_num <- 100:120
+vector_num
+
+# get the number from vector position 6
+vector_num[6]
+
+# get the range of numbers from 7 to 14 from vector
+vector_num[7:14]
+
