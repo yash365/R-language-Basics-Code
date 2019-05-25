975dde60df3a889626c485dc73ff66e43cbee858
yashj365 <yash.jain6350@gmail.com>
2019-05-25 23:46:10 +0530

    adding matrix basics

---
+++ Code/how_to_use_matrix.R
@@ -0,0 +1,22 @@
+# matrix having value till 100 and divided in 10 rows
+one <- matrix(1:100, nrow = 11)
+one
+
+# having two simliar martrix
+two <- matrix(10:15, nrow = 3)
+two
+
+three <- matrix(200:205, nrow = 3)
+three
+
+# tells how many row and columns is there in the matrix
+dim(one)
+dim(two)
+dim(three)
+
+# adding two similar matirx
+two + three
+
+# multiplying two similar matrix
+two * three
+
