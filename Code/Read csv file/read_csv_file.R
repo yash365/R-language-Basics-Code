0a51adba6bdd62facdffe7d67a956d20f8ce2272
yashj365 <yash.jain6350@gmail.com>
2019-05-30 23:51:59 +0530

    read the csv file through local storage and through link

---
+++ Code/Read csv file/read_csv_file.R
@@ -0,0 +1,23 @@
+# get the working directory
+getwd()
+
+# copy sample_data.csv in working directory in my case it n the documents
+sampleData <- read.csv("sample_data.csv", FALSE, ",")
+
+# data type 
+class(sampleData)
+
+# display top 5 data
+head(sampleData)
+
+# display last 5 data
+tail(sampleData)
+
+# read the csv file through link
+sampleData2 <- read.csv("http://insight.dev.schoolwires.com/HelpAssets/C2Assets/C2Files/C2ImportCalEventSample.csv", TRUE, ",")
+
+# data type 
+class(sampleData2)
+
+# display top data
+sampleData2
